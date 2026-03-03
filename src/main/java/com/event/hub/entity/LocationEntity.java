a0b546d8840ae14e88e6793223b684a66084b013
Shumakov Ilya <shumakoff.ilya@yandex.ru>
2026-03-03 16:39:23 +0300

    feat: SuperEntity

--- src/main/java/com/event/hub/entity/LocationEntity.java
+++ src/main/java/com/event/hub/entity/LocationEntity.java
@@ -2,9 +2,6 @@ package com.event.hub.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,10 +17,7 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class LocationEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class LocationEntity extends SuperEntity {
 
     @Column(name = "name", nullable = false, unique = true)
     private String name;
---
+++ src/main/java/com/event/hub/entity/SuperEntity.java
@@ -0,0 +1,13 @@
+package com.event.hub.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class SuperEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
