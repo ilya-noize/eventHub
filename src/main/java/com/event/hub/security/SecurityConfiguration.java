30f0032cddd4cda506c332264e0b13809abe84f3
Shumakov Ilya <shumakoff.ilya@yandex.ru>
2026-03-25 20:47:37 +0300

    event secure and exception handling

--- src/main/java/com/event/hub/security/SecurityConfiguration.java
+++ src/main/java/com/event/hub/security/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.event.hub.security;
 
 import com.event.hub.db.entity.UserRole;
+import com.event.hub.exception.RestAccessDeniedHandler;
+import com.event.hub.exception.RestAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,20 +15,28 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
     private final CustomUserDetailsService customUserDetailsService;
     private final JwtTokenFilter jwtTokenFilter;
+    private final AccessDeniedHandler restAccessDeniedHandler;
+    private final AuthenticationEntryPoint restAuthenticationEntryPoint;
 
     public SecurityConfiguration(
             CustomUserDetailsService customUserDetailsService,
-            JwtTokenFilter jwtTokenFilter
+            JwtTokenFilter jwtTokenFilter,
+            RestAccessDeniedHandler restAccessDeniedHandler,
+            RestAuthenticationEntryPoint restAuthenticationEntryPoint
     ) {
         this.customUserDetailsService = customUserDetailsService;
         this.jwtTokenFilter = jwtTokenFilter;
+        this.restAccessDeniedHandler = restAccessDeniedHandler;
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
     }
 
     @Bean
@@ -34,29 +44,46 @@ public class SecurityConfiguration {
         String admin = UserRole.ADMIN.name();
         String user = UserRole.USER.name();
         security
-                .formLogin(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .authorizeHttpRequests(auth ->
-                        auth
-                                .requestMatchers("/swagger-ui/**").permitAll()
+            .formLogin(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/swagger-ui/**").permitAll()
 
-                                .requestMatchers(HttpMethod.POST, "/users").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/users/**").hasAuthority(admin)
+                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/users/my").hasAuthority(user)
+                .requestMatchers(HttpMethod.GET, "/users/**").hasAuthority(admin)
 
-                                .requestMatchers(HttpMethod.POST, "/locations").hasAuthority(admin)
-                                .requestMatchers(HttpMethod.PUT, "/locations/**").hasAuthority(admin)
-                                .requestMatchers(HttpMethod.PATCH, "/locations/**").hasAuthority(admin)
-                                .requestMatchers(HttpMethod.DELETE, "/locations/**").hasAuthority(admin)
-                                .requestMatchers("/locations/**").hasAnyAuthority(admin, user)
+                .requestMatchers(HttpMethod.POST, "/locations").hasAuthority(admin)
+                .requestMatchers(HttpMethod.PUT, "/locations/**").hasAuthority(admin)
+                .requestMatchers(HttpMethod.PATCH, "/locations/**").hasAuthority(admin)
+                .requestMatchers(HttpMethod.DELETE, "/locations/**").hasAuthority(admin)
+                .requestMatchers("/locations/**").hasAnyAuthority(admin, user)
 
-                                .anyRequest().authenticated()
+                .requestMatchers(HttpMethod.POST, "/events").hasAuthority(user)
+                .requestMatchers(HttpMethod.POST, "/events/search").hasAnyAuthority(admin, user)
+                .requestMatchers(HttpMethod.PUT, "/events/{eventId}").hasAnyAuthority(admin, user)
+                .requestMatchers(HttpMethod.PATCH, "/events/{eventId}").hasAuthority(admin)
+                .requestMatchers(HttpMethod.DELETE, "/events/{eventId}").hasAnyAuthority(admin, user)
+                .requestMatchers(HttpMethod.GET, "/events/{eventId}").hasAnyAuthority(admin,user)
+                .requestMatchers(HttpMethod.GET, "/events/my").hasAuthority(user)
 
-                )
-                .addFilterBefore(jwtTokenFilter, AnonymousAuthenticationFilter.class);
+                .requestMatchers(HttpMethod.POST, "/events/registrations/{eventId}").hasAuthority(user)
+                .requestMatchers(HttpMethod.GET, "/events/registrations/cancel/{eventId}").hasAuthority(user)
+                .requestMatchers(HttpMethod.GET, "/events/registrations/my").hasAuthority(user)
+
+                .requestMatchers("/events/**").hasAnyAuthority(admin, user)
+
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(jwtTokenFilter, AnonymousAuthenticationFilter.class)
+            .exceptionHandling(handle -> handle
+                    .accessDeniedHandler(restAccessDeniedHandler)
+                    .authenticationEntryPoint(restAuthenticationEntryPoint)
+            );
 
         return security.build();
     }
