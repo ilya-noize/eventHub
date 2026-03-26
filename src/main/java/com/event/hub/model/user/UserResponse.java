434af936235d4252b748154905fc0705a01606d3
Shumakov Ilya <shumakoff.ilya@yandex.ru>
2026-03-26 13:59:24 +0300

    add role

--- src/main/java/com/event/hub/model/user/UserResponse.java
+++ src/main/java/com/event/hub/model/user/UserResponse.java
@@ -1,8 +1,11 @@
 package com.event.hub.model.user;
 
+import com.event.hub.db.entity.UserRole;
+
 public record UserResponse(
         Long id,
         String login,
-        String age
+        String age,
+        UserRole role
 ) {
 }
