b1be5fec0f5c8e7a69cafb72d0ee1736631aad3d
Shumakov Ilya <shumakoff.ilya@yandex.ru>
2026-03-26 13:58:39 +0300

    init users from json

--- src/main/java/com/event/hub/service/initializer/DefaultUserInitializer.java
+++ src/main/java/com/event/hub/service/initializer/DefaultUserInitializer.java
@@ -2,17 +2,26 @@ package com.event.hub.service.initializer;
 
 import com.event.hub.db.entity.UserRole;
 import com.event.hub.model.user.User;
+import com.event.hub.service.LocationService;
 import com.event.hub.service.UserService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @Component
 @Profile("dev")
 @RequiredArgsConstructor
 public class DefaultUserInitializer {
     private final UserService userService;
+
+    private final LocationService locationService;
 
     @PostConstruct
     public void init() {
@@ -32,6 +41,22 @@ public class DefaultUserInitializer {
                         .role(UserRole.USER.name())
                         .build()
         );
+        createFiftyUserFromJson();
+    }
+
+    private void createFiftyUserFromJson() {
+        String userJson = "user.json";
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            ClassPathResource resource = new ClassPathResource(userJson);
+            try (InputStream inputStream = resource.getInputStream()) {
+                List<User> users = mapper.readValue(inputStream,
+                        mapper.getTypeFactory().constructCollectionType(List.class, User.class));
+                users.forEach(this::createUserIfNotExists);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("File " + userJson + " not found:" + e.getMessage(), e);
+        }
     }
 
     private void createUserIfNotExists(User user) {
